d00ea43b179302289202000af1b7f0d7af3b9208
MaartjeHoltslag <mcsholtslag@gmail.com>
2021-12-29 15:14:52 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from distutils.core import setup
 setup(
     name = 'esrinl-utils',
     packages = ['esrinl-utils'],
-    version = 'version number',  # Ideally should be same as your GitHub release tag varsion
+    version = '0.1',
     description = 'A set of common functions to be used in scripts',
-    author = '',
-    author_email = '',
-    url = 'github package source url',
-    download_url = 'download link you saved',
-    keywords = ['tag1', 'tag2'],
-    classifiers = [],
+    author = 'EsriNL DevTeam',
+    author_email = 'developers@esri.nl',
+    license='MIT',
+    url = 'https://github.com/esrinederland/PythonUtils',
+    download_url = 'https://github.com/esrinederland/PythonUtils/archive/refs/tags/0.1.tar.gz'
 )
