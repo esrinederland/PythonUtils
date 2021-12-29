535d7c797d2d480b13906d6f8772e732b9e20efe
MaartjeHoltslag <mcsholtslag@gmail.com>
2021-12-29 15:32:13 +0100

    V0.2

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name = 'esrinlutils',
     packages = ['esrinlutils'],
-    version = '0.1',
+    version = '0.2',
     description = 'A set of common functions to be used in scripts',
     author = 'EsriNL DevTeam',
     author_email = 'developers@esri.nl',
     license='MIT',
     url = 'https://github.com/esrinederland/PythonUtils',
-    download_url = 'https://github.com/esrinederland/PythonUtils/archive/refs/tags/0.1.tar.gz'
+    download_url = 'https://github.com/esrinederland/PythonUtils/archive/refs/tags/0.2.tar.gz'
 )
